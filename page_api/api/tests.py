e4313e09d74636954765bdaa539b6a985bca67b9
billk97 <vasileios@konstantinou.dev>
2021-11-20 18:30:56 +0200

    add requirements.txt and fix .gitignore

--- page_api/api/tests.py
+++ page_api/api/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from page_api.api.calc import add
+
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        self.assertEqual(add(3, 5), 8)
